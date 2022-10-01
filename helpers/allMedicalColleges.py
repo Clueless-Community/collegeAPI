169b7bf047651b26063f6e10b463e3b900eeffca
deathsurgeon1 <schaudhary0104@gmail.com>
2022-10-01 11:54:51 +0530

    endpoints to fetch all the medical colleges

---
+++ helpers/allMedicalColleges.py
@@ -0,0 +1,30 @@
+# Scrape all the medical colleges
+
+import pandas as pd
+import json
+
+scrapper = pd.read_html(
+    'https://www.nirfindia.org/2022/MedicalRankingALL.html')
+
+df = scrapper[0]
+
+test = df.iloc[2:, :].values
+testList = test.tolist()
+
+
+def convert_to_dict(list):
+    keysName = ['name', 'city', 'state']
+    dictValue = dict(zip(keysName, list))
+    return dictValue
+
+collegesList = []
+for i in range(len(df)):
+    testList = df.iloc[i:, :].values
+    testList2 = testList.tolist()
+    testDict = convert_to_dict(testList2[0])
+    collegesList.append(testDict)
+
+# collegesList
+json_file = json.dumps(collegesList, indent=4)
+with open('../data/allMedicalColleges.json', 'a') as file:
+    file.write(json_file)
