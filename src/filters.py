aec782929ad9aeb422ac05f72627e3e33b7f8fb2
root <smit16@somaiya.edu>
2022-10-03 05:15:53 +0000

    fix: delimeter separation on state and city filters

--- src/filters.py
+++ src/filters.py
@@ -1,6 +1,5 @@
 # Imports
-import json
-
+import json, os
 
 # Engineering Colleges Filter
 def engineering_colleges_by_state(state):
@@ -26,3 +25,29 @@ def engineering_colleges_by_city(city):
         if cityCheck == city:
             output.append(data[i])
     return output
+    
+
+# Medical Colleges Filter
+def medical_colleges_by_state(state):
+    
+    with open(r'data\allMedicalColleges.json', 'r') as file:
+        data = json.load(file)
+    output = []
+    for i in range(len(data)):
+        stateCheck = data[i]['state'].replace(" ", "").lower()
+        if stateCheck == state:
+            output.append(data[i])
+    return output
+
+
+def medical_colleges_by_city(city):
+    
+    with open(r'data\allMedicalColleges.json', 'r') as file:
+        data = json.load(file)
+    output = []
+    for i in range(len(data)):
+        cityCheck = data[i]['city'].replace(" ", "").lower()
+        if cityCheck == city:
+            output.append(data[i])
+    return output
+    
