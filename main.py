c3aa3f5b498fdcf3e8a055421716dbb700b28fff
Ashutosh Krishna <ashutoshbritish@gmail.com>
2022-10-06 22:44:30 +0530

    Add /colleges route

--- main.py
+++ main.py
@@ -199,3 +199,15 @@ async def managementCollegesByState(state: str or None = None):
     except Exception as e:
         raise HTTPException(
             status_code=404, detail='Some error occured, please try again')
+
+
+@app.get('/colleges')
+def allColleges():
+
+    try:
+        with open(r'data\allEngineeringColleges.json', 'r') as file:
+            output = json.load(file)
+    except:
+        raise HTTPException(status_code=404)
+
+    return output
